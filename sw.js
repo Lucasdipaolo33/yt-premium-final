3363ef43b4434812e6fa865540919b9206010d69
Lucasdipaolo33 <dpgodoycruz@gmail.com>
2026-02-14 07:57:49 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
 const BLACKLIST = [
     'doubleclick.net', 'googleads.g.doubleclick.net', 'googlesyndication.com',
     'adservice.google.com', 'googleadservices.com', 'youtube.com/api/stats/ads',
-    'youtube.com/pagead', 'video-stats.l.google.com', 'ad_status', 'ptracking',
-    'stats/ads', 'api/stats/qoe', '/log_event?el=ad'
+    'youtube.com/pagead', 'video-stats.l.google.com', 'ad_status', 'ptracking'
 ];
 
 self.addEventListener('fetch', (event) => {
